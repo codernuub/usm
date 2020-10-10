ec553e9d840423b8caf77d86dc4234f7bb10e9d2
codernuub <sardarbhai6@gmail.com>
2020-10-10 20:18:45 +0530

    testing media11

--- client/js/script.js
+++ client/js/script.js
@@ -1,11 +1,11 @@
-window.onload = function () {
-  const lecture = document.querySelector('.lecture');
-  const video = document.querySelector('video');
-  video.videoWidth = lecture.clientWidth;
-  video.videoHeight = lecture.clientHeight
-}
-const cameraOptions = document.querySelector('.video-options>select');
-const buttons = [...document.querySelectorAll('button')];
+const lecture = document.querySelector(".lecture");
+const video = document.querySelector("video");
+
+video.videoWidth = lecture.clientWidth;
+video.videoHeight = lecture.clientHeight;
+
+const cameraOptions = document.querySelector(".video-options>select");
+const buttons = [...document.querySelectorAll("button")];
 let streamStarted = false;
 
 const [play, pause, stop] = buttons;
@@ -19,35 +19,34 @@ const videoConfig = {
   height: {
     min: 720,
     ideal: 1080,
-    max: 1440
-  }
-}
+    max: 1440,
+  },
+};
 
 play.onclick = () => {
   if (streamStarted) {
     video.play();
     return;
   }
-  if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
-    consoleOnDom('msg', `requesting ${cameraOptions.value}`)
+  if ("mediaDevices" in navigator && navigator.mediaDevices.getUserMedia) {
+    consoleOnDom("msg", `requesting ${cameraOptions.value}`);
     const constraints = {
       video: {
         ...videoConfig,
-        facingMode: 'environment'
-      }
+        facingMode: "environment",
+      },
     };
     startStream(constraints);
   }
 };
 
-//get requested media 
+//get requested media
 const startStream = async (constraints, requestedDeviceChange) => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia(constraints);
     handleStream(stream, requestedDeviceChange);
-  }
-  catch (err) {
-    consoleOnDom('error', err.message)
+  } catch (err) {
+    consoleOnDom("error", err.message);
   }
 };
 //output video on video element
@@ -62,20 +61,19 @@ cameraOptions.onchange = () => {
   try {
     /*stop current camera stream here*/
     if (window.stream)
-      window.stream.getTracks().forEach(track => track.stop())
+      window.stream.getTracks().forEach((track) => track.stop());
 
     /*stop current camera stream here*/
-    consoleOnDom('msg', `requesting ${cameraOptions.value}`)
+    consoleOnDom("msg", `requesting ${cameraOptions.value}`);
     const constraints = {
       video: {
         ...videoConfig,
-        facingMode: 'user'
-      }
+        facingMode: "user",
+      },
     };
     startStream(constraints);
-  }
-  catch (err) {
-    consoleOnDom('error', err.message)
+  } catch (err) {
+    consoleOnDom("error", err.message);
   }
 };
 
@@ -83,4 +81,3 @@ cameraOptions.onchange = () => {
 const pauseStream = () => video.pause();
 
 pause.onclick = pauseStream;
-
