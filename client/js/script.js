2ead4ef42042bd356ad7fd3ad1de2f08387c7115
codernuub <sardarbhai6@gmail.com>
2020-10-10 20:02:00 +0530

    testing media8

--- client/js/script.js
+++ client/js/script.js
@@ -1,73 +1,80 @@
-try {
-  const cameraOptions = document.querySelector('.video-options>select');
-  const video = document.querySelector('video');
-  const buttons = [...document.querySelectorAll('button')];
-  let streamStarted = false;
 
-  const [play, pause, stop] = buttons;
+const cameraOptions = document.querySelector('.video-options>select');
+const video = document.querySelector('video');
+const buttons = [...document.querySelectorAll('button')];
+let streamStarted = false;
 
-  const constraints = {
-    video: {
-      width: {
-        min: 1280,
-        ideal: 1920,
-        max: 2560,
-      },
-      height: {
-        min: 720,
-        ideal: 1080,
-        max: 1440
-      },
-    }
-  };
+const [play, pause, stop] = buttons;
 
-  //get all video input source
-  const getCameraSelection = async () => {
-    const devices = await navigator.mediaDevices.enumerateDevices();
-    const videoDevices = devices.filter(device => device.kind === 'videoinput');
-    const options = videoDevices.map(videoDevice => {
-      return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
-    });
-    cameraOptions.innerHTML = options.join('');
-  };
+const constraints = {
+  video: {
+    width: {
+      min: 1280,
+      ideal: 1920,
+      max: 2560,
+    },
+    height: {
+      min: 720,
+      ideal: 1080,
+      max: 1440
+    },
+  }
+};
 
-  play.onclick = () => {
-    if (streamStarted) {
-      video.play();
-      return;
-    }
-    if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
-      consoleOnDom('msg', `requesting ${cameraOptions.value}`)
-      const updatedConstraints = {
-        ...constraints,
-        deviceId: {
-          exact: cameraOptions.value
-        }
-      };
-      startStream(updatedConstraints, false);
-    }
-  };
+//get all video input source
+const getCameraSelection = async () => {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const videoDevices = devices.filter(device => device.kind === 'videoinput');
+  const options = videoDevices.map(videoDevice => {
+    return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
+  });
+  cameraOptions.innerHTML = options.join('');
+};
 
-  //get requested media 
-  const startStream = async (constraints, requestedDeviceChange) => {
+play.onclick = () => {
+  if (streamStarted) {
+    video.play();
+    return;
+  }
+  if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
+    consoleOnDom('msg', `requesting ${cameraOptions.value}`)
+    const updatedConstraints = {
+      ...constraints,
+      deviceId: {
+        exact: cameraOptions.value
+      }
+    };
+    startStream(updatedConstraints, false);
+  }
+};
+
+//get requested media 
+const startStream = async (constraints, requestedDeviceChange) => {
+  try {
     const stream = await navigator.mediaDevices.getUserMedia(constraints);
     handleStream(stream, requestedDeviceChange);
-  };
-  //output video on video element
-  const handleStream = (stream, requestedDeviceChange) => {
-    video.srcObject = stream;
-    window.stream = stream;
-    streamStarted = true;
-    if (requestedDeviceChange) video.play();
-  };
+  }
+  catch (err) {
+    consoleOnDom('error', err.message)
+  }
+};
+//output video on video element
+const handleStream = (stream, requestedDeviceChange) => {
+  video.srcObject = stream;
+  window.stream = stream;
+  streamStarted = true;
+  if (requestedDeviceChange) video.play();
+};
 
-  getCameraSelection();
+getCameraSelection();
 
 
-  //camera change
-  cameraOptions.onchange = () => {
+//camera change
+cameraOptions.onchange = () => {
+  try {
     /*stop current camera stream here*/
     window.stream.getTracks().forEach(track => track.stop())
+
     /*stop current camera stream here*/
     consoleOnDom('msg', `requesting ${cameraOptions.value}`)
     const updatedConstraints = {
@@ -77,14 +84,14 @@ try {
       }
     };
     startStream(updatedConstraints, true);
-  };
+  }
+  catch (err) {
+    consoleOnDom('error', err.message)
+  }
+};
 
-  //pause video
-  const pauseStream = () => video.pause();
+//pause video
+const pauseStream = () => video.pause();
 
-  pause.onclick = pauseStream;
+pause.onclick = pauseStream;
 
-}
-catch (err) {
-  consoleOnDom('error', err.message);
-}
